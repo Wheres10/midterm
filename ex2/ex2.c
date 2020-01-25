cf43c95e37a04bfc97f9628174e5d1a3a03e4eec
Wheres10 <56293497+Wheres10@users.noreply.github.com>
2020-01-25 16:09:56 +0800

    Update ex2.c

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,43 +1,46 @@
 #include <stdio.h>
 int main()
 {
-    unsigned int a,b;
-    double d;
+    float a,b,d;
 
     char c;
 
     a=0;
     b=0;
 
-    while ((c=getchar())!='\n'){
-        a=a*10+c-'0';
+    while ((c=getchar())!=' '){
+        a=a * 10 + c - '0';
     }
 
-    while ((c=getchar())!='\n'){
-        b=b*10+c-'0';
+    while ((c=getchar())!=' '){
+        b=b * 10 + c - '0';
     }
 
     c=getchar();
     switch(c){
         case'+':
         d=a+b;
+        printf("The expression has a number of %.1f\n",d);
         break;
         case'-':
         d=a-b;
+        printf("The expression has a number of %.1f\n",d);
         break;
         case'*':
         d=a*b;
+        printf("The expression has a number of %.1f\n",d);
         break;
         case'/':
         if(b==0){
-            puts("Error:connot divide a number by 0");
+            printf("Error:connot divide a number by 0");
             break;
         }
-        else{d=1.0*a/b;}
-  break;}
-  if(b!=0){
-      printf("The expression has a value of :%.1f\n",d);
-  }
-  getchar ();
-  return 0;
+        else
+        {
+            d=a/b;
+            printf("The expression has a number of %.1f\n",d);
+        }
+        
+}
+return 0;
 }
